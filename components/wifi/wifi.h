b087ed564dd40abdf66ccc4e8530244956a19bd9
OliverEros <olivereros@protonmail.com>
2025-01-18 20:59:14 +0000

    Exposed new functions - promiscOn, print_scanned_AP, get_target_mac_address

--- components/wifi/wifi.h
+++ components/wifi/wifi.h
@@ -8,7 +8,17 @@
 #include "driver/uart.h"
 #include "esp_log.h"
 
+// struct FormattedPayload {
+//     uint8_t FrameControl;
+//     uint8_t Duration;
+//     uint8_t RA;
+//     uint8_t TA;
+//     uint8_t SeqCtl;
+// };
 
 void wifi_scan(void);
 void initialise_wifi(void);
 void deauth(void);
+uint8_t get_target_mac_address(uint8_t targetId);
+void print_scanned_AP(void);
+void PromiscOn(void);
